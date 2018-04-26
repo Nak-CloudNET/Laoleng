90a657b47d912304f44189329931b0243fc3f713
Channthy <lychanthy2017@gmail.com>
2018-04-26 10:20:46 +0700

    #Chanthy

--- erp/models/Sales_model.php
+++ erp/models/Sales_model.php
@@ -40,7 +40,7 @@ class Sales_model extends CI_Model
 				}
 			}
         } else {
-            $this->db->where("(products.track_quantity = 0 OR warehouses_products.quantity > 0) AND warehouses_products.warehouse_id = '" . $warehouse_id . "' AND "
+            $this->db->where("warehouses_products.warehouse_id = '" . $warehouse_id . "' AND "
                 . "(name LIKE '%" . $term . "%' OR code LIKE '%" . $term . "%' OR  concat(name, ' (', code, ')') LIKE '%" . $term . "%') AND inactived <> 1");
 			if($this->Owner || $this->admin){
 				if($warehouse_id != ""){
@@ -807,7 +807,8 @@ class Sales_model extends CI_Model
         }
         return FALSE;
     }
-	 public function getAllReturnsItem($sale_id)
+
+    public function getAllReturnsItem($sale_id)
     {
         $this->db->select('return_items.*, tax_rates.code as tax_code, tax_rates.name as tax_name, tax_rates.rate as tax_rate, (CASE WHEN erp_products.unit = 0 THEN erp_products.unit ELSE erp_units.name END) as uname, products.details as details, product_variants.name as variant, products.unit, products.promotion, categories.name AS category_name')
             ->join('products', 'products.id=return_items.product_id', 'left')
@@ -815,6 +816,7 @@ class Sales_model extends CI_Model
             ->join('tax_rates', 'tax_rates.id=return_items.tax_rate_id', 'left')
 			->join('categories', 'categories.id = products.category_id', 'left')
             ->join('units', 'units.id = products.unit', 'left')
+            ->where('return_items.subtotal >', 0)
             ->group_by('return_items.id')
             ->order_by('id', 'asc');
         $q = $this->db->get_where('return_items', array('return_id' => $sale_id));
